c0d5a703636e7f28635b4ac4aa14a024a049cab0
Fistname Lastname <email@example.com>
2023-12-13 11:26:12 +0100

    Finished the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const questionsArray = [
-    ["Quelle est la ville fictive où se déroulent la plupart des aventures de Batman ?", [0, "Metropolis"], [1, "Gotham City"], [0, "Central City"], [0, "Star City"]],
     ["Quel est le vrai nom de Batman ?", [0, "Maurice Lee"], [0, "Lex Luthor"], [1, "Bruce Wayne"], [0, "Stan Lee"]],
+    ["Quelle est la ville fictive où se déroulent la plupart des aventures de Batman ?", [0, "Metropolis"], [1, "Gotham City"], [0, "Central City"], [0, "Star City"]],
     ["Quel est le nom du fidèle majordome de Bruce Wayne ?", [0, "Alfred Pennywise"], [1, "Alfred Pennyworth"], [0, "Alfred Hitchcock"], [0, "Alfred Molina"]],
     ["Quel super-vilain est connu pour être l'ennemi juré de Batman, avec un visage défiguré ?", [0, "Le Sphinx"], [0, "Double-Face"], [1, "Le Joker"], [0, "L'Épouvantail"]],
     ["Quelle Batgirl est la fille du commissaire de police James Gordon ?", [0, "Lady Gaga"], [0, "Helena Bertinelli"], [1, "Barbara Gordon"], [0, "Selina Kyle"]],
@@ -13,6 +13,10 @@ const questionsArray = [
 
 
 const questions = document.querySelector("#questions");
+const hidden = document.querySelector("#hidden");
+const perso2 = document.querySelector("#perso1");
+const result = document.querySelector("#result");
+const perso1 = document.querySelector("#perso");
 const nbr = document.querySelector("#nbr-qst");
 const asw0 = document.querySelector("#asw-0");
 const asw1 = document.querySelector("#asw-1");
@@ -22,32 +26,39 @@ const asw3 = document.querySelector("#asw-3");
 let index = 0;
 let score = 0;
 
-function checkAnswer(nbr){
-    if (questionsArray[index][nbr][0] === 1){
+function checkAnswer(nbr) {
+    if (questionsArray[index][nbr][0] === 1) {
         score++;
-        index++;
-    } else {
-        index++;
     }
+    index++;
+    displayQuestion(index)
 }
 
-if (index < questionsArray.length) {
-    nbr.innerText = "Question " + (index + 1);
-    questions.innerText = questionsArray[index][0];
-    asw0.innerText = questionsArray[index][1][1];
-    asw1.innerText = questionsArray[index][2][1];
-    asw2.innerText = questionsArray[index][3][1];
-    asw3.innerText = questionsArray[index][4][1];
-    asw0.addEventListener('click', function(e){
-        checkAnswer(1);
-    });
-    asw1.addEventListener('click', function(e){
-        checkAnswer(2);
-    });
-    asw2.addEventListener('click', function(e){
-        checkAnswer(3);
-    });
-    asw3.addEventListener('click', function(e){
-        checkAnswer(4);
-    });
+function displayQuestion(index) {
+    if (index < questionsArray.length) {
+        nbr.innerText = "Question " + (index + 1);
+        questions.innerText = questionsArray[index][0];
+        asw0.innerText = questionsArray[index][1][1];
+        asw1.innerText = questionsArray[index][2][1];
+        asw2.innerText = questionsArray[index][3][1];
+        asw3.innerText = questionsArray[index][4][1];
+    } else {
+        hidden.classList.remove("hidden");
+        result.innerText = (score === 1) ? "Vous avez 1 bonne réponse sur 10": "Vous avez " + score + " bonnes réponses sur 10" ;
+        perso1.innerText = (score <= 5) ? "Le Joker est fier de vous" : "Batman compte sur vous pour la relève";
+        perso2.src = (score <= 5) ? "assets/img/joker.gif" : "assets/img/batman1.gif";
+    }
 };
+
+asw0.addEventListener('click', function (e) {
+    checkAnswer(1);
+});
+asw1.addEventListener('click', function (e) {
+    checkAnswer(2);
+});
+asw2.addEventListener('click', function (e) {
+    checkAnswer(3);
+});
+asw3.addEventListener('click', function (e) {
+    checkAnswer(4);
+});
